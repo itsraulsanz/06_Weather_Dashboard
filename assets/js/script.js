e61643271ced3920668cd238fa7f1b1200c009d8
Raul Sanz <luar_znas@hotmail.com>
2021-03-28 19:17:40 +0100

    fixing duplications a new city is print; fixed the city list links and prevented the City duplicates in the localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
-var API_KEY = "4be23710c7753951da2356832f750589";
-var asideCities = document.querySelector(".aside");
+var formEl = document.querySelector(".city-form");
 var userCity = document.querySelector("#city");
 var cityListEl = document.querySelector(".cityList");
 var containerEl = document.querySelector(".containerWeather");
@@ -8,62 +7,93 @@ var fiveDaysWeatherEl = document.querySelector(".fiveDaysWeather");
 var currentWeatherTitle = document.querySelector(".currentWeatherTitle");
 var currentWeatherContent = document.querySelector(".currentWeatherContent");
 
+var API_KEY = "4be23710c7753951da2356832f750589";
 
-$(".searchButton").click(function (event) {
+function search(event) {
+  var cityName;
+  console.log(event)
   event.preventDefault();
-  var cityName = userCity.value.trim();
-  containerEl.style.display = "block";
-  var cityResultName = "";
+  if(event.type === "load"){
+    console.log("load", event.type)
+    var cityList = JSON.parse(localStorage.getItem("cityList")) || [];
+    if(cityList.length === 0){
+      return;
+    }
+    console.log(cityList[cityList.length - 1].city)
+    cityName = cityList[cityList.length - 1].city;
+  } else {
+
+    var cityName = userCity.value.trim();
+    var cityResultName = "";
+  }
 
   saveUserCity(userCity.value);
   renderCities();
 
   function saveUserCity(city) {
     var userCity = {
-      city: city
+      city: city,
     };
-    var cityList = JSON.parse(window.localStorage.getItem("cityList")) || [];
-  
-    cityList.push(userCity);
-  
-    window.localStorage.setItem("cityList", JSON.stringify(cityList));
+    var cityList = JSON.parse(localStorage.getItem("cityList")) || [];
+
+    var shouldBeAdded = true;
+    for (var i = 0; i < cityList.length; i++) {
+      if (cityList[i].city.toLowerCase() === city.toLowerCase() || city.length === 0 ) {
+        shouldBeAdded = false;
+      }
+    }
+    if (shouldBeAdded) {
+      cityList.push(userCity);
+
+      localStorage.setItem("cityList", JSON.stringify(cityList));
+    }
   }
-  
+
   // Showing the cityList
   function renderCities() {
     cityListEl.style.display = "block";
-  
+    cityListEl.innerHTML = "";
+
     var userCityList = document.createElement("ul");
-  
+
     cityListEl.appendChild(userCityList);
-    
-    var cityList = JSON.parse(window.localStorage.getItem("cityList")) || [];
-  
+
+    var cityList = JSON.parse(localStorage.getItem("cityList")) || [];
+
     for (let i = 0; i < cityList.length; i++) {
       var li = document.createElement("li");
+      //var a = document.createElement("a");
       li.textContent = cityList[i].city;
+      li.addEventListener("click", function (event) {
+        console.log(event.target.textContent);
+        getOpenWeatherData(event.target.textContent);
+      });
+      //a.href = "#";
       userCityList.appendChild(li);
+      //li.appendChild(a);
     }
-  
+
     // clear City search
     var clearCityButton = document.createElement("button");
     clearCityButton.textContent = "Clear the search list";
     cityListEl.appendChild(clearCityButton);
-  
+
     clearCityButton.addEventListener("click", function () {
       localStorage.clear();
       cityListEl.innerHTML = "";
     });
   }
 
-  function getOpenWeatherData() {
-    return fetch(
+  function getOpenWeatherData(city) {
+    console.log("called", city);
+    fetch(
       "http://api.openweathermap.org/geo/1.0/direct?q=" +
-      cityName +
-      "&limit=1&appid=" +
-      API_KEY
+        city +
+        "&limit=1&appid=" +
+        API_KEY
     )
       .then(function (res) {
+        console.log(res);
         return res.json();
       })
       .then(function (response) {
@@ -73,16 +103,19 @@ $(".searchButton").click(function (event) {
 
         return fetch(
           "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-          lat +
-          "&lon=" +
-          lon +
-          "&exclude=minutely,hourly&appid=" +
-          API_KEY +
-          "&units=metric"
+            lat +
+            "&lon=" +
+            lon +
+            "&exclude=minutely,hourly&appid=" +
+            API_KEY +
+            "&units=metric"
         );
       })
       .then(function (res) {
         return res.json();
+      })
+      .then((data) => {
+        printCityWeatherData(data);
       });
   }
 
@@ -100,8 +133,9 @@ $(".searchButton").click(function (event) {
   }
 
   function printCityWeatherData(weatherResponse) {
-    console.log(weatherResponse);
+    //console.log(weatherResponse);
     //console.log({ cityResultName });
+
     currentWeatherTitle.textContent = cityResultName;
     var date = formatUNIXDate(weatherResponse.daily[0].dt);
     var icon = weatherResponse.current.weather[0].icon;
@@ -123,14 +157,12 @@ $(".searchButton").click(function (event) {
     currentWeatherHum.textContent = "Humidity: " + humidity + " %";
     currentWeatherWind.textContent = "Wind Speed: " + windSpeed + " MPH";
     currentWeatherUV.textContent = "UV Index: " + uvi;
-
-    //console.log(currentWeatherTemp);
-
+    currentWeatherContent.innerHTML = "";
     currentWeatherContent.appendChild(currentWeatherTemp);
     currentWeatherContent.appendChild(currentWeatherHum);
     currentWeatherContent.appendChild(currentWeatherWind);
     currentWeatherContent.appendChild(currentWeatherUV);
-
+    fiveDaysWeatherEl.innerHTML = "";
     for (var i = 1; i <= 5; i++) {
       var dayForecastBox = document.createElement("div");
       dayForecastBox.classList.add(
@@ -162,9 +194,17 @@ $(".searchButton").click(function (event) {
     }
   }
 
-  getOpenWeatherData().then(printCityWeatherData);
+  getOpenWeatherData(cityName);
   // .catch(function (error) {
   //   console.log(error);
   //   alert("Unable to connect to openweathermap");
   // });
-});
+}
+
+formEl.addEventListener("submit",(event) => {
+  search(event)
+} );
+
+window.addEventListener("load", (event) => {
+  search(event)
+})
